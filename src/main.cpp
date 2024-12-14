4dbfd0c0f4a24f1cac6acccd1a0f3b4517404e34
Jacob Signorovitch <jacob.signorovitch@gmail.com>
2024-12-13 20:39:37 -0500

    The.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include "main.h"
 
-
 using namespace ez;
-
 
 inline pros::Motor intake(1);
 inline pros::Motor hook_conveyor(17);
@@ -11,15 +9,13 @@ inline ez::Piston mogo_mech('A');
 Drive chassis (
     {-14, -15, -16}, //Left Motor Ports
     {11, 12 ,13},
-    
+
     //Right Motor Ports
     7, //IMU Port
     3.25, //Wheel Size
     600, // rpm
     1.3333
 );
-
-
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -29,57 +25,53 @@ Drive chassis (
  */
 void initialize() {
 
+    pros::delay(
+        500); // Stop the user from doing anything while legacy ports configure
 
-    pros::delay(500); // Stop the user from doing anything while legacy ports configure
-
-
-    chassis.opcontrol_curve_buttons_toggle(false); // Enables modifying the controller curve with buttons on the joysticks
-    chassis.opcontrol_drive_activebrake_set(0); // Sets the active brake kP. We recommend 2.
-    chassis.opcontrol_curve_default_set(1, 7);
+    chassis.opcontrol_curve_buttons_toggle(
+        false); // Enables modifying the controller curve with buttons on the
+                // joysticks
+    chassis.opcontrol_drive_activebrake_set(
+        0); // Sets the active brake kP. We recommend 2.
+    chassis.opcontrol_curve_default_set(1, 5);
     default_constants(); // Set the drive to your own constants from autons.cpp!
 
     // as::auton_selector.autons_add({
-    //     Auton("Example Drive\n\nDrive forward and come back.", drive_example),
-    //     Auton("Example Turn\n\nTurn 3 times.", turn_example),
-    //     Auton("Drive and Turn\n\nDrive forward, turn, come back. ", drive_and_turn),
-    //     Auton("Drive and Turn\n\nSlow down during drive.", wait_until_change_speed),
-    //     Auton("Swing Example\n\nSwing in an 'S' curve", swing_example),
-    //     Auton("Combine all 3 movements", combining_movements),
-    //     Auton("Interference\n\nAfter driving forward, robot performs differently if interfered or not.", interfered_example),
+    //     Auton("Example Drive\n\nDrive forward and come back.",
+    //     drive_example), Auton("Example Turn\n\nTurn 3 times.", turn_example),
+    //     Auton("Drive and Turn\n\nDrive forward, turn, come back. ",
+    //     drive_and_turn), Auton("Drive and Turn\n\nSlow down during drive.",
+    //     wait_until_change_speed), Auton("Swing Example\n\nSwing in an 'S'
+    //     curve", swing_example), Auton("Combine all 3 movements",
+    //     combining_movements), Auton("Interference\n\nAfter driving forward,
+    //     robot performs differently if interfered or not.",
+    //     interfered_example),
     // });
-
 
     chassis.initialize();
     as::initialize();
     master.rumble(".");
 }
 
+void disabled() {}
 
-void disabled() {
-
-}
-
-void competition_initialize() {
-
-}
+void competition_initialize() {}
 
 void autonomous() {
-    chassis.pid_targets_reset(); // Resets PID targets to 0
-    chassis.drive_imu_reset(); // Reset gyro position to 0
-    chassis.drive_sensor_reset(); // Reset drive sensors to 0
-    chassis.drive_brake_set(MOTOR_BRAKE_HOLD); // Set motors to hold.  This helps autonomous consistency
-
+    chassis.pid_targets_reset();               // Resets PID targets to 0
+    chassis.drive_imu_reset();                 // Reset gyro position to 0
+    chassis.drive_sensor_reset();              // Reset drive sensors to 0
+    chassis.drive_brake_set(MOTOR_BRAKE_HOLD); // Set motors to hold.  This
+                                               // helps autonomous consistency
 
     int dist = 48;
     chassis.pid_drive_set(dist, 127, true);
     chassis.pid_wait_until(dist - 5);
 
-    int dist2 = -(dist-6);
+    int dist2 = -(dist - 6);
     chassis.pid_drive_set(dist2, 127, true);
     chassis.pid_wait_until(dist2 + 5);
 }
-
-
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -97,53 +89,44 @@ void autonomous() {
 void opcontrol() {
 
     chassis.drive_brake_set(MOTOR_BRAKE_COAST);
-    //swing_example();
+    // swing_example();
     while (true) {
-        
-        
 
-        if (!pros::competition::is_connected()) { 
-            if (master.get_digital_new_press(DIGITAL_X)) 
+        if (!pros::competition::is_connected()) {
+            if (master.get_digital_new_press(DIGITAL_X))
                 chassis.pid_tuner_toggle();
-                
 
-            if (master.get_digital_new_press(DIGITAL_B)) 
-                autonomous();
+            // This code cost the lives of twelve orphans.
+            // if (master.get_digital_new_press(DIGITAL_B))
+            //    autonomous();
 
             chassis.pid_tuner_iterate(); // Allow PID Tuner to iterate
-        } 
+        }
 
         if (master.get_digital(DIGITAL_R1)) {
-            intake.move(127);
-        } 
-        else if (master.get_digital(DIGITAL_R2)) {
             intake.move(-127);
-        } 
-        else {
+        } else if (master.get_digital(DIGITAL_R2)) {
+            intake.move(127);
+        } else {
             hook_conveyor.move(0);
         }
 
         if (master.get_digital(DIGITAL_L1)) {
             intake.move(127);
             hook_conveyor.move(-127);
-        } 
-        else if (master.get_digital(DIGITAL_L2)) {
+        } else if (master.get_digital(DIGITAL_L2)) {
             intake.move(-127);
             hook_conveyor.move(127);
-        } 
-        else {
+        } else {
             intake.move(0);
         }
 
-        
-       
-
-       mogo_mech.button_toggle(master.get_digital(DIGITAL_A)); // 'A' button for mogo
-
+        mogo_mech.button_toggle(
+            master.get_digital(DIGITAL_A)); // 'A' button for mogo
 
         chassis.opcontrol_arcade_standard(SPLIT); // Arcade control
 
-
-        pros::delay(util::DELAY_TIME); // This is used for timer calculations!  Keep this util::DELAY_TIME
+        pros::delay(util::DELAY_TIME); // This is used for timer calculations!
+                                       // Keep this util::DELAY_TIME
     }
 }
