bfb674d2121d5be1553d6fb6d91ce7bd1feb52d1
Jacob Signorovitch <jacob.signorovitch@gmail.com>
2024-12-14 09:13:40 -0500

    Fool.

--- include/globals.h
+++ include/globals.h
@@ -1,11 +1,9 @@
-
-#include "main.h"
-
 #ifndef GLOBALS
 #define GLOBALS
 
+#include "main.h"
 extern pros::Motor intake;
 extern pros::Motor hook_conveyor;
 extern ez::Piston mogo_mech;
 
-#endif GLOBALS
+#endif
